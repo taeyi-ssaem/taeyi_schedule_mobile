3b816acdd6d5ee018ebdf3dec4242d0d1296cb24
taeyi-ssaem <queellin@gmail.com>
2026-06-17 20:31:19 +0900

    지난 일정 맨 아래로 + 기본 접힘(보기 토글)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 일정비서 모바일(보기 전용) 서비스워커
-const CACHE = "iljeong-mobile-v1";
+const CACHE = "iljeong-mobile-v2";
 const SHELL = ["./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install", e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate", e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
